f48bc074979865d2d568e054f8aa60cb244d4d60
Samuel Mullen <samullen@gmail.com>
2009-07-28 12:31:40 -0500

    changed some of the defaults around. Added test_attribute_changes and test_lifecycle

--- test/basicdaemon_test.rb
+++ test/basicdaemon_test.rb
@@ -6,23 +6,38 @@ require 'test/unit'
 class BasicDaemonTest < Test::Unit::TestCase
 
 def setup
-  @daemon = BasicDaemon.new
+  @daemon = BasicDaemon.new({:piddir => '/tmp', :workingdir => '/tmp'})
 end
 
 def test_generic_init
-  assert_equal '/var/lock/basicdaemon_test', @daemon.pidfile
-  assert_equal '/var/lock', @daemon.piddir
-  assert_equal '/', @daemon.workingdir
+  d = BasicDaemon.new
+
+  assert_equal '/tmp/basicdaemon_test', d.pidpath
+  assert_equal '/tmp', d.piddir
+  assert_equal '/', d.workingdir
 end
 
 def test_specific_init
-  d = BasicDaemon.new({:pidfile => 'foo', :piddir => '/tmp', :workingdir => '.'})
-  assert_equal '/tmp', d.piddir
-  assert_equal '/tmp/foo', d.pidfile
-  assert_equal '.', d.workingdir
+  d = BasicDaemon.new({:pidfile => 'foo', :piddir => '/var/lock', :workingdir => '/var/lock'})
+  assert_equal '/var/lock', d.piddir
+  assert_equal '/var/lock/foo', d.pidpath
+  assert_equal '/var/lock', d.workingdir
 end
 
-def test_start
+def test_attribute_changes
+  d = BasicDaemon.new({:pidfile => "foo.txt"})
+
+  assert_equal '/tmp', d.piddir = '/tmp'
+  assert_equal '/tmp/foo.txt', d.pidpath
+  d.pidfile = "bar.txt"
+  assert_equal '/tmp/bar.txt', d.pidpath
+  assert_equal '/etc', d.workingdir = '/etc'
+end
+
+def test_lifecycle
+  d = BasicDaemon.new
+  d.start
+  d.stop
 end
 
 end
