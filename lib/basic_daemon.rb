0864212e220ecc18f46167de177d4b327279f1b0
Samuel Mullen <samullen@gmail.com>
2009-12-30 08:08:50 -0600

    Updated version and history

--- lib/basic_daemon.rb
+++ lib/basic_daemon.rb
@@ -2,7 +2,7 @@ class BasicDaemon
   attr_accessor :workingdir, :pidfile, :piddir
   attr_reader :pid
 
-  VERSION = '0.1.2'
+  VERSION = '0.1.3'
 
   DEFAULT_OPTIONS = {
     :pidfile => File.basename($PROGRAM_NAME, File.extname($PROGRAM_NAME)),
